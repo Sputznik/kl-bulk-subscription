c2550ce40303b7644984fb525f505a9fd842ec2d
Guneet Narula <guneetnarula@sputznik.com>
2022-04-04 13:17:59 +0530

    Updated email instructions

--- lib/templates/coupon-email.php
+++ lib/templates/coupon-email.php
@@ -1,5 +1,5 @@
 <p><strong>You are in!</strong> Use the discount code below to avail 100% discount on an annual subscription to KL Hatke.</p>
 <p><strong><?php _e( $coupon_code );?></strong></p>
-<p>You can checkout your subscription here: <?php _e( $checkout_url );?></p>
+<p>You can enter this code on the following checkout page and register your account: <?php _e( $checkout_url );?></p>
 <p>Your premium subscription ensures that we continue to invest in women as journalists, you ensure that ‘her story creates history’. Thank you for your continued support towards Khabar Lahariya and stay tuned for what's coming next.</p>
 <p>-- Khabar Lahariya</p>
